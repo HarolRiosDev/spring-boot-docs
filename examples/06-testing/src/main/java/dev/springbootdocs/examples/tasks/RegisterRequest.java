5893f353dcc35e32f7a602fdef3f2c094f594ec6
HRDev <125bharrio@gmail.com>
2026-09-23 18:37:09 +0200

    chore: port examples/04-cache-redis source verbatim into examples/06-testing

---
+++ examples/06-testing/src/main/java/dev/springbootdocs/examples/tasks/RegisterRequest.java
@@ -0,0 +1,9 @@
+package dev.springbootdocs.examples.tasks;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+        @NotBlank String username,
+        @NotBlank @Size(min = 8) String password) {
+}
